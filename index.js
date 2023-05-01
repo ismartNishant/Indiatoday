096be315b2b04572615b8204fb2e82a14978d368
ismartNishant <nishantrajputall@gmail.com>
2023-05-01 15:45:11 +0530

    initial layout ready

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-// const slider = document.querySelector('.parent');
-// let mouseDown = false;
-// let startX, scrollLeft;
 
-// let startDragging = function (e) {
-//   mouseDown = true;
-//   startX = e.pageX - slider.offsetLeft;
-//   scrollLeft = slider.scrollLeft;
-// };
-// let stopDragging = function (event) {
-//   mouseDown = false;
-// };
 
-// slider.addEventListener('mousemove', (e) => {
-//   e.preventDefault();
-//   if(!mouseDown) { return; }
-//   const x = e.pageX - slider.offsetLeft;
-//   const scroll = x - startX;
-//   slider.scrollLeft = scrollLeft - scroll;
-// });
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
 
-// // Add the event listeners
-// slider.addEventListener('mousedown', startDragging, false);
-// slider.addEventListener('mouseup', stopDragging, false);
-// sl
+$(document).ready(function(){
+    $("#close").click(function(){
+      $(".left-div").hide();
+    });
+    $(".my-menu").click(function(){
+      $(".left-div").show();
+    });
+  });
