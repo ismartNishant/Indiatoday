ee0059cb61d1c70d26c2db11e2f3001805c1ebbe
ismartNishant <nishantrajputall@gmail.com>
2023-05-05 20:54:47 +0530

    project Done

--- index.js
+++ index.js
@@ -65,12 +65,6 @@ var swiper3 = new Swiper(".mySwiper3", {
   autoHeight: true,
   slidesPerView: "auto",
   grabCursor: true,
-
-  // pagination: {
-  //   el: ".swiper-pagination3",
-  //   clickable: true,
-
-  // },
   breakpoints: {
     centeredSlides: true,
   },
@@ -86,12 +80,6 @@ var swiper4 = new Swiper(".mySwiper4", {
   autoHeight: true,
   slidesPerView: "auto",
   grabCursor: true,
-
-  // pagination: {
-  //   el: ".swiper-pagination3",
-  //   clickable: true,
-
-  // },
   breakpoints: {
     centeredSlides: true,
   },
@@ -132,40 +120,135 @@ slider.addEventListener("mousemove", (e) => {
 });
 
 
-
-
-// to hide and show left div
-
-if (window.innerWidth <= 912) {
-  const closeButton = document.querySelector("#close");
-  const myMenu = document.querySelectorAll(".my-menu");
-  const leftDiv = document.querySelector(".left-div");
-
-  for (var i = 0; i < myMenu.length; i++) {
-    myMenu[i].addEventListener("click", function () {
-      if (leftDiv.style.display === "none") {
-        leftDiv.style.display = "block";
-      } else {
-        leftDiv.style.display = "none";
-      }
-    });
+//--------------------------------for bs5 theme follow below code-----------------------------------------
+(function () {
+  let lightSwitch = document.getElementById("lightSwitch");
+  if (!lightSwitch) {
+    return;
   }
 
-  window.onresize = function () {
-      leftDiv.style.display = "block ";
-  };
-  
-  closeButton.addEventListener("click", function () {
-    leftDiv.style.display = "none";
-  });
+  /**
+   * @function darkmode
+   * @summary: changes the theme to 'dark mode' and save settings to local stroage.
+   * Basically, replaces/toggles every CSS class that has '-light' class with '-dark'
+   */
+  function darkMode() {
+    document.querySelectorAll(".bg-light").forEach((element) => {
+      element.className = element.className.replace(/-light/g, "-dark");
+    });
 
-  // document.addEventListener("mouseup", function (e) {
-  //   var container = leftDiv;
+    document.querySelectorAll(".link-dark").forEach((element) => {
+      element.className = element.className.replace(/link-dark/, "text-white");
+    });
 
-  //   if (!container.contains(e.target)) {
-  //     container.style.display = "none";
-  //   }
-  // });
-}
+    document.body.classList.add("bg-dark");
+
+    if (document.body.classList.contains("text-dark")) {
+      document.body.classList.replace("text-dark", "text-light");
+    } else {
+      document.body.classList.add("text-light");
+    }
+
+    // Tables
+    var tables = document.querySelectorAll("table");
+    for (var i = 0; i < tables.length; i++) {
+      // add table-dark class to each table
+      tables[i].classList.add("table-dark");
+    }
+
+    // set light switch input to true
+    if (!lightSwitch.checked) {
+      lightSwitch.checked = true;
+    }
+    localStorage.setItem("lightSwitch", "dark");
+  }
+
+  /**
+   * @function lightmode
+   * @summary: changes the theme to 'light mode' and save settings to local stroage.
+   */
+  function lightMode() {
+    document.querySelectorAll(".bg-dark").forEach((element) => {
+      element.className = element.className.replace(/-dark/g, "-light");
+    });
+
+    document.querySelectorAll(".text-white").forEach((element) => {
+      element.className = element.className.replace(/text-white/, "link-dark");
+    });
+
+    document.body.classList.add("bg-light");
+
+    if (document.body.classList.contains("text-light")) {
+      document.body.classList.replace("text-light", "text-dark");
+    } else {
+      document.body.classList.add("text-dark");
+    }
+
+    // Tables
+    var tables = document.querySelectorAll("table");
+    for (var i = 0; i < tables.length; i++) {
+      if (tables[i].classList.contains("table-dark")) {
+        tables[i].classList.remove("table-dark");
+      }
+    }
+
+    if (lightSwitch.checked) {
+      lightSwitch.checked = false;
+    }
+    localStorage.setItem("lightSwitch", "light");
+  }
+
+  /**
+   * @function onToggleMode
+   * @summary: the event handler attached to the switch. calling @darkMode or @lightMode depending on the checked state.
+   */
+  function onToggleMode() {
+    let elem = document.getElementById("mode");
+    if (lightSwitch.checked) {
+      darkMode();
+
+      if (elem.innerText == "LIGHT MODE") {
+        elem.innerText = "DARK MODE";
+      }
+      else elem.innerText = "LIGHT MODE";
+    } else {
+      lightMode();
+      if (elem.innerText == "LIGHT MODE") {
+        elem.innerText = "DARK MODE";
+      }
+      else elem.innerText = "LIGHT MODE";
+    }
+  }
+
+  /**
+   * @function getSystemDefaultTheme
+   * @summary: get system default theme by media query
+   */
+  function getSystemDefaultTheme() {
+    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    if (darkThemeMq.matches) {
+      return "dark";
+    }
+    return "light";
+  }
+
+  function setup() {
+    var settings = localStorage.getItem("lightSwitch");
+    if (settings == null) {
+      settings = getSystemDefaultTheme();
+    }
+
+    if (settings == "dark") {
+      lightSwitch.checked = true;
+    }
+
+    lightSwitch.addEventListener("change", onToggleMode);
+    onToggleMode();
+  }
+
+  setup();
+})();
+
+
 
 
